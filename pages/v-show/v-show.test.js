b496f0a3b8e3c9e67bebc559f014bbcc31de0ced
hdx <hdx@dcloud.io>
2023-06-28 18:14:23 +0800

    feat: v-show:: style screenshot

--- pages/v-show/v-show.test.js
+++ pages/v-show/v-show.test.js
@@ -1,14 +1,34 @@
-const PAGE_PATH = '/pages/v-slot/v-slot'
+const PAGE_PATH = '/pages/v-show/v-show'
 
-describe('v-slot', () => {
+describe('v-show', () => {
   let page
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor(500)
   })
-  it('list-items-3', async () => {
-    const length = 3;
-    const elements = await page.$$('.list-item')
-    expect(elements.length).toBe(length)
+  it('style::display', async () => {
+    const element = await page.$('.hello')
+    expect(await element.style('display')).toBe('flex')
+
+    const toggle = await page.$('.toggle')
+    await toggle.tap()
+    expect(await element.style('display')).toBe('none')
+    await toggle.tap()
+    expect(await element.style('display')).toBe('flex')
+  })
+  it('screenshot', async () => {
+    const toggle = await page.$('.toggle')
+    const element = await page.$('.hello')
+
+    await program.screenshot({
+      path: "static/v-show/screenshot-show1.png"
+    })
+
+    await toggle.tap()
+
+    await toggle.tap()
+    await program.screenshot({
+      path: "static/v-show/screenshot-show2.png"
+    })
   })
 })
