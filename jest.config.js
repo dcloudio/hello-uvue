d3ca3e750b44075475613cd88e334f003274303c
DCloud__JSON <linju@dcloud.io>
2023-06-30 21:14:18 +0800

    新增的state状态的自动化测试例

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
     watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
     moduleFileExtensions: ['js', 'json'],
     rootDir: __dirname,
-    testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
+    testMatch: ["<rootDir>/pages/state/**/*.test.js"],
     testPathIgnorePatterns: ['/node_modules/']
 }
