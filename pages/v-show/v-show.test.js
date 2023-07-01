94d7ba9bfe5b6b8b411b667130e6f76dbf4a4f4c
hdx <hdx@dcloud.io>
2023-07-01 15:35:17 +0800

    v-show: add screenshot test

--- pages/v-show/v-show.test.js
+++ pages/v-show/v-show.test.js
@@ -7,28 +7,30 @@ describe('v-show', () => {
     await page.waitFor(500)
   })
   it('style::display', async () => {
-    const element = await page.$('.hello')
+    const element = await page.$('.v-show-content')
     expect(await element.style('display')).toBe('flex')
 
-    const toggle = await page.$('.toggle')
+    const toggle = await page.$('.btn-toggle')
     await toggle.tap()
     expect(await element.style('display')).toBe('none')
     await toggle.tap()
     expect(await element.style('display')).toBe('flex')
   })
   it('screenshot', async () => {
-    const toggle = await page.$('.toggle')
+    const toggle = await page.$('.btn-toggle')
     const element = await page.$('.hello')
 
-    await program.screenshot({
-      path: "static/v-show/screenshot-show1.png"
-    })
+    const image1 = await program.screenshot()
+    expect(image1).toMatchSnapshot()
 
     await toggle.tap()
+    await page.waitFor(20)
+    const image2 = await program.screenshot()
+    expect(image2).toMatchSnapshot()
 
     await toggle.tap()
-    await program.screenshot({
-      path: "static/v-show/screenshot-show2.png"
-    })
+    await page.waitFor(20)
+    const image3 = await program.screenshot()
+    expect(image3).toMatchSnapshot()
   })
 })
