2f9738eb8021229ab791068a780355408e8899bf
hdx <hdx@dcloud.io>
2023-07-03 16:17:48 +0800

    v-for: add test file (v-for-item-click v-for-item-v-if v-for-item-v-show)

--- pages/directive/v-for/v-for-item-click.js
+++
@@ -1,9 +0,0 @@
-const PAGE_PATH = '/pages/directive/v-for/v-for-item-click'
-
-describe('v-for-item-click', () => {
-  let page
-  beforeAll(async () => {
-    page = await program.reLaunch(PAGE_PATH)
-    await page.waitFor(500)
-  })
-})
--- pages/directive/v-for/v-for-item-v-if.js
+++
@@ -1,9 +0,0 @@
-const PAGE_PATH = '/pages/directive/v-for/v-for-item-v-if'
-
-describe('v-for-item-v-if', () => {
-  let page
-  beforeAll(async () => {
-    page = await program.reLaunch(PAGE_PATH)
-    await page.waitFor(500)
-  })
-})
