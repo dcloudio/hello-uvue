263dd9248692aae64a864c72eab37c0ce49acfca
zhenyuWang <13641039885@163.com>
2023-07-13 18:08:55 +0800

    perf(lifecycle): add annotation

--- App.vue
+++ App.vue
@@ -1,121 +1,129 @@
 <script lang="ts">
-  import { state, setLifeCycleNum } from './store/index.uts'
-  export default {
-    onLaunch: function () {
-      setLifeCycleNum(state.lifeCycleNum + 1000)
-      console.log('App Launch')
+import { state, setLifeCycleNum } from './store/index.uts'
+export default {
+  onLaunch: function () {
+    // 自动化测试
+    setLifeCycleNum(state.lifeCycleNum + 1000)
+    console.log('App Launch')
 
-      const performance : Performance = uni.getPerformance()
-      const observer : PerformanceObserver = performance.createObserver((entryList : PerformanceObserverEntryList) => {
-        console.log("observer:entryList.getEntries()")
+    const performance: Performance = uni.getPerformance()
+    const observer: PerformanceObserver = performance.createObserver(
+      (entryList: PerformanceObserverEntryList) => {
+        console.log('observer:entryList.getEntries()')
         console.log(entryList.getEntries())
-      })
-      observer.observe({ entryTypes: ['render', 'navigation'] } as PerformanceObserverOptions)
-    },
-    onShow: function () {
-      setLifeCycleNum(state.lifeCycleNum + 100)
-      console.log('App Show')
-    },
-    onHide: function () {
-      setLifeCycleNum(state.lifeCycleNum - 100)
-      console.log('App Hide')
-    },
-    onLastPageBackPress: function (): boolean | null {
-      setLifeCycleNum(state.lifeCycleNum - 1000)
-      uni.showToast({
-        title: '再按一次退出应用',
-        position: 'bottom'
-      })
-      return null
-    },
-  }
+      }
+    )
+    observer.observe({
+      entryTypes: ['render', 'navigation'],
+    } as PerformanceObserverOptions)
+  },
+  onShow: function () {
+    // 自动化测试
+    setLifeCycleNum(state.lifeCycleNum + 100)
+    console.log('App Show')
+  },
+  onHide: function () {
+    // 自动化测试
+    setLifeCycleNum(state.lifeCycleNum - 100)
+    console.log('App Hide')
+  },
+  onLastPageBackPress: function (): boolean | null {
+    // 自动化测试
+    setLifeCycleNum(state.lifeCycleNum - 1000)
+    uni.showToast({
+      title: '再按一次退出应用',
+      position: 'bottom',
+    })
+    return null
+  },
+}
 </script>
 
 <style>
-  /* @font-face {
+/* @font-face {
     font-family: uniicons;
     font-weight: normal;
     font-style: normal;
     src: url('./static/fonts/uni.ttf') format('truetype');
   } */
 
-  .page {
-    padding: 15px;
-  }
+.page {
+  padding: 15px;
+}
 
-  .uni-panel {
-    margin-bottom: 12px;
-  }
+.uni-panel {
+  margin-bottom: 12px;
+}
 
-  .uni-panel-h {
-    background-color: #ffffff;
-    flex-direction: row;
-    align-items: center;
-    padding: 12px;
-  }
+.uni-panel-h {
+  background-color: #ffffff;
+  flex-direction: row;
+  align-items: center;
+  padding: 12px;
+}
 
-  .uni-panel-h-on {
-    background-color: #f0f0f0;
-  }
+.uni-panel-h-on {
+  background-color: #f0f0f0;
+}
 
-  .uni-panel-text {
-    color: #000000;
-    font-size: 14px;
-    font-weight: normal;
-  }
+.uni-panel-text {
+  color: #000000;
+  font-size: 14px;
+  font-weight: normal;
+}
 
-  .uni-panel-icon {}
+.uni-panel-icon {}
 
-  .uni-panel-icon-on {
-    transform: rotate(180deg);
-  }
+.uni-panel-icon-on {
+  transform: rotate(180deg);
+}
 
-  .uni-navigate-item {
-    flex-direction: row;
-    align-items: center;
-    background-color: #FFFFFF;
-    border-top-style: solid;
-    border-top-color: #f0f0f0;
-    border-top-width: 1px;
-    padding: 12px;
-    justify-content: space-between;
-  }
+.uni-navigate-item {
+  flex-direction: row;
+  align-items: center;
+  background-color: #ffffff;
+  border-top-style: solid;
+  border-top-color: #f0f0f0;
+  border-top-width: 1px;
+  padding: 12px;
+  justify-content: space-between;
+}
 
-  .uni-navigate-item:active {
-    background-color: #f8f8f8;
-  }
+.uni-navigate-item:active {
+  background-color: #f8f8f8;
+}
 
-  .uni-navigate-text {
-    color: #000000;
-    font-size: 12px;
-    opacity: 0.8;
-  }
+.uni-navigate-text {
+  color: #000000;
+  font-size: 12px;
+  opacity: 0.8;
+}
 
-  .uni-navigate-icon {
-    margin-left: 15px;
-    color: #999999;
-    font-size: 14px;
-    font-weight: normal;
-  }
+.uni-navigate-icon {
+  margin-left: 15px;
+  color: #999999;
+  font-size: 14px;
+  font-weight: normal;
+}
 
-  .text-disabled {
-    color: #a0a0a0;
-  }
+.text-disabled {
+  color: #a0a0a0;
+}
 
-  .list-item-text {
-    line-height: 36px;
-  }
+.list-item-text {
+  line-height: 36px;
+}
 
-  .split-title {
-    margin: 20px 0 5px;
-    padding: 5px 0;
-    border-bottom: 1px solid #dfdfdf;
-  }
+.split-title {
+  margin: 20px 0 5px;
+  padding: 5px 0;
+  border-bottom: 1px solid #dfdfdf;
+}
 
-  .btn-view {
-    margin: 10px 0;
-    padding: 10px;
-    border: 1px solid #dfdfdf;
-    border-radius: 3px;
-  }
+.btn-view {
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid #dfdfdf;
+  border-radius: 3px;
+}
 </style>
