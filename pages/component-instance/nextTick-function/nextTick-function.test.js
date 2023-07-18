b608220f71588f674f1c92542cb6ba31a5ef52aa
chenruilong <chen1195849487@hotmail.com>
2023-07-18 15:22:41 +0800

    增加 $nextTick() 子组件测试用例

--- pages/component-instance/nextTick-function/nextTick-function.test.js
+++ pages/component-instance/nextTick-function/nextTick-function.test.js
@@ -20,4 +20,20 @@ describe('$nextTick()', () => {
     expect(data.nextTickBeforeHeight).toBe(100)
     expect(data.nextTickAfterHeight).toBe(200)
   });
+
+  it('子组件 $nextTick() 回调方式', async () => {
+    const comp = await page.$('.next-tick-child')
+    const data = await comp.data()
+
+    expect(data.nextTickBeforeWidth).toBe(100)
+    expect(data.nextTickAfterWidth).toBe(200)
+  });
+
+  it('子组件 $nextTick() Promise方式', async () => {
+    const comp = await page.$('.next-tick-child')
+    const data = await comp.data()
+
+    expect(data.nextTickBeforeHeight).toBe(100)
+    expect(data.nextTickAfterHeight).toBe(200)
+  });
 })
