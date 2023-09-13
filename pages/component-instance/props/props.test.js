df7390fcef202c387700d91155958d8223b48531
hdx <hdx@dcloud.io>
2023-09-13 18:08:48 +0800

    props: 补充已支持的数据类型, array, object

--- pages/component-instance/props/props.test.js
+++ pages/component-instance/props/props.test.js
@@ -11,9 +11,13 @@ describe('$props', () => {
     const string = await page.$('.string')
     const number = await page.$('.number')
     const boolean = await page.$('.boolean')
+    const arrayString = await page.$('.array-string')
+    const object = await page.$('.object')
 
     expect(await string.text()).toBe('abcd')
     expect(await number.text()).toBe('12345')
     expect(await boolean.text()).toBe('true')
+    expect(await arrayString.text()).toBe('str1')
+    expect(await object.text()).toBe('1')
   })
 })
