78dd38ebf8fe79eb49a25ba0847d8a5e6c1d9dce
hdx <hdx@dcloud.io>
2023-11-23 14:32:41 +0800

    chore: 处理代码警告问题

--- components/call-easy-method/call-easy-method.vue
+++ components/call-easy-method/call-easy-method.vue
@@ -1,19 +1,23 @@
 <template>
-  <view></view>
+  <view>{{result}}</view>
 </template>
 
 <script>
   export default {
     data() {
       return {
+        result: ''
       }
     },
     methods: {
       foo1() {
+        this.result = "foo1"
       },
       foo2(date1 : number) {
+        this.result = "foo2=" + date1
       },
       foo3(date1 : number, date2 : number) {
+        this.result = "foo3=" + date1 + " " + date2
       },
       foo4(callback : (() => void)) {
         callback()
