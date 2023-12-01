0dc46350d9a0f463fa8f9850fe1a1a67e19d002e
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-01 15:12:06 +0800

    test: 调整测试例兼容 web 端

--- pages/state/props/props.test.js
+++ pages/state/props/props.test.js
@@ -5,7 +5,7 @@ describe('/pages/state/props/props', () => {
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
     page = await program.currentPage();
-    console.log('page', page);
+    // console.log('page', page);
     await page.waitFor(1000);
   });
 
@@ -13,19 +13,15 @@ describe('/pages/state/props/props', () => {
     let str = await getMyComponentText("#str")
     console.log('str-*--*-',str);
     expect(str).toEqual("你好我是props.uvue组件传进的数据");
-  
     let num = await getMyComponentText("#num")
     // console.log('num-*--*-',num);
     expect(parseInt(num)).toEqual(1);
-    
     page.callMethod('addNum')
     num = await getMyComponentText("#num")
     // console.log('num-*--*-',num);
     expect(parseInt(num)).toEqual(2);
   });
 });
-
-
 async function getMyComponentText(param){
-  return await(await(await page.$('my-component')).$(param)).text();
+  return await(await(await page.$('.my-component')).$(param)).text();
 }
