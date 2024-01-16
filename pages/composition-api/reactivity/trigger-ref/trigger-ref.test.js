3199ca6a381d1a2a5cb59eb12fd2790001f9eb45
zhenyuWang <13641039885@163.com>
2024-01-16 20:18:10 +0800

    feat(composition api): triggerRef

---
+++ pages/composition-api/reactivity/trigger-ref/trigger-ref.test.js
@@ -0,0 +1,29 @@
+const PAGE_PATH = '/pages/composition-api/reactivity/trigger-ref/trigger-ref'
+
+describe('triggerRef', () => {
+  if (process.env.uniTestPlatformInfo.startsWith('android')) {
+    let page = null
+    beforeAll(async () => {
+      page = await program.reLaunch(PAGE_PATH)
+      await page.waitFor('view')
+    })
+    it('basic', async () => {
+      const stateCount = await page.$('#state-count')
+      expect(await stateCount.text()).toBe('state.count: 0')
+
+      const incrementStateCountBtn = await page.$('#increment-state-count-btn')
+      await incrementStateCountBtn.tap()
+
+      expect(await stateCount.text()).toBe('state.count: 0')
+
+      const triggerRefStateBtn = await page.$('#trigger-ref-state-btn')
+      await triggerRefStateBtn.tap()
+
+      expect(await stateCount.text()).toBe('state.count: 1')
+    })
+  } else {
+    it('other platform', () => {
+      expect(1).toBe(1)
+    })
+  }
+})
