a16a03acac8622bd214a4403ec00ae2c2d424d2c
zhenyuWang <13641039885@163.com>
2024-01-17 16:07:19 +0800

    feat(composition api): defineSlots

---
+++ pages/composition-api/basic/define-slots/define-slots.test.js
@@ -0,0 +1,25 @@
+const PAGE_PATH = '/pages/composition-api/basic/define-slots/define-slots'
+
+describe('defineSlots', () => {
+  if (process.env.uniTestPlatformInfo.startsWith('android')) {
+    let page = null
+    beforeAll(async () => {
+      page = await program.reLaunch(PAGE_PATH)
+      await page.waitFor('view')
+    })
+    it('basic', async () => {
+      const slotHeader = await page.$('#slot-header')
+      expect(await slotHeader.text()).toBe('header slot msg: foo msg')
+      
+      const slotContent = await page.$('#slot-default')
+      expect(await slotContent.text()).toBe('default slot num: 0')
+      
+      const slotFooter = await page.$('#slot-footer')
+      expect(await slotFooter.text()).toBe('footer slot arr: ["a","b","c"]')
+    })
+  } else {
+    it('other platform', () => {
+      expect(1).toBe(1)
+    })
+  }
+})
