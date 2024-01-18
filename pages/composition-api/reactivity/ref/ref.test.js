4d404eb0f88a944e5fae7639ea8017ad91a45e32
zhenyuWang <13641039885@163.com>
2024-01-18 20:25:53 +0800

    feat(composition api): 调整示例及测试兼容  web

--- pages/composition-api/reactivity/ref/ref.test.js
+++ pages/composition-api/reactivity/ref/ref.test.js
@@ -1,30 +1,25 @@
 const PAGE_PATH = '/pages/composition-api/reactivity/ref/ref'
 
 describe('ref', () => {
-  if (process.env.uniTestPlatformInfo.startsWith('android')) {
-    let page = null
-    beforeAll(async () => {
-      page = await program.reLaunch(PAGE_PATH)
-      await page.waitFor('view')
-    })
-    it('basic', async () => {
-      const count1 = await page.$('#count1')
-      expect(await count1.text()).toBe('count1: 0')
-      const count2 = await page.$('#count2')
-      expect(await count2.text()).toBe('count2: 0')
-      const counterCount = await page.$('#counter-count')
-      expect(await counterCount.text()).toBe('counter.count: 0')
+  let page = null
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor('view')
+  })
+  it('basic', async () => {
+    const count1 = await page.$('#count1')
+    expect(await count1.text()).toBe('count1: 0')
+    const count2 = await page.$('#count2')
+    expect(await count2.text()).toBe('count2: 0')
+    const counterCount = await page.$('#counter-count')
+    expect(await counterCount.text()).toBe('counter.count: 0')
 
-      const incrementBtn = await page.$('#increment-btn')
-      await incrementBtn.tap()
+    const incrementBtn = await page.$('.increment-btn')
+    await incrementBtn.tap()
 
-      expect(await count1.text()).toBe('count1: 2')
-      expect(await count2.text()).toBe('count2: 2')
-      expect(await counterCount.text()).toBe('counter.count: 1')
-    })
-  } else {
-    it('other platform', () => {
-      expect(1).toBe(1)
-    })
-  }
+    expect(await count1.text()).toBe('count1: 2')
+    expect(await count2.text()).toBe('count2: 2')
+    expect(await counterCount.text()).toBe('counter.count: 1')
+  })
+
 })
--- pages/composition-api/reactivity/un-ref/un-ref.test.js
+++ pages/composition-api/reactivity/un-ref/un-ref.test.js
@@ -1,26 +1,20 @@
 const PAGE_PATH = '/pages/composition-api/reactivity/un-ref/un-ref'
 
 describe('unref', () => {
-  if (process.env.uniTestPlatformInfo.startsWith('android')) {
-    let page = null
-    beforeAll(async () => {
-      page = await program.reLaunch(PAGE_PATH)
-      await page.waitFor('view')
-    })
-    it('basic', async () => {
-      const refCount = await page.$('#ref-count')
-      expect(await refCount.text()).toBe('ref count: 0')
-      const refCountType = await page.$('#ref-count-type')
-      expect(await refCountType.text()).toBe('ref count type: object')
+  let page = null
+  beforeAll(async () => {
+    page = await program.reLaunch(PAGE_PATH)
+    await page.waitFor('view')
+  })
+  it('basic', async () => {
+    const refCount = await page.$('#ref-count')
+    expect(await refCount.text()).toBe('ref count: 0')
+    const refCountType = await page.$('#ref-count-type')
+    expect(await refCountType.text()).toBe('ref count type: object')
 
-      const count = await page.$('#count')
-      expect(await count.text()).toBe('count: 0')
-      const isRefCount = await page.$('#count-type')
-      expect(await isRefCount.text()).toBe('count type: number')
-    })
-  } else {
-    it('other platform', () => {
-      expect(1).toBe(1)
-    })
-  }
+    const count = await page.$('#count')
+    expect(await count.text()).toBe('count: 0')
+    const isRefCount = await page.$('#count-type')
+    expect(await isRefCount.text()).toBe('count type: number')
+  })
 })
