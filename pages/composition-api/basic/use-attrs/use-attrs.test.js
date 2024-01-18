b11a004bead720189f1d3230000925553ca99553
zhenyuWang <13641039885@163.com>
2024-01-18 15:53:57 +0800

    feat(composition api): useAttrs

---
+++ pages/composition-api/basic/use-attrs/use-attrs.test.js
@@ -0,0 +1,22 @@
+const PAGE_PATH = '/pages/composition-api/basic/use-attrs/use-attrs'
+
+describe('useAttrs', () => {
+  if (process.env.uniTestPlatformInfo.startsWith('android')) {
+    let page = null
+    beforeAll(async () => {
+      page = await program.reLaunch(PAGE_PATH)
+      await page.waitFor('view')
+    })
+    it('basic', async () => {
+      const attrsClass = await page.$('#attrs-class')
+      expect(await attrsClass.text()).toBe('attrs.class: foo')
+
+      const attrsMsg = await page.$('#attrs-msg')
+      expect(await attrsMsg.text()).toBe('attrs.msg: msg')
+    })
+  } else {
+    it('other platform', () => {
+      expect(1).toBe(1)
+    })
+  }
+})
