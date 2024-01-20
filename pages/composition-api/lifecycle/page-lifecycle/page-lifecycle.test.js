145e39a287747e1149103783d67d7aa4194767f9
zhenyuWang <13641039885@163.com>
2024-01-20 16:20:25 +0800

    feat: 页面生命周期示例及测试

--- pages/composition-api/lifecycle/page-lifecycle/page-lifecycle.test.js
+++ pages/composition-api/lifecycle/page-lifecycle/page-lifecycle.test.js
@@ -1,30 +1,15 @@
-const PAGE_PATH = '/pages/lifecycle/page/page'
+const PAGE_PATH = '/pages/composition-api/lifecycle/page-lifecycle/page-lifecycle'
 const HOME_PATH = '/pages/tab-bar/options-api'
 const INTER_PAGE_PATH = '/pages/app-instance/index/index'
 let page
 let lifeCycleNum
 
-describe('app-lifecycle', () => {
-	it('onLaunch onShow', async () => {
-		page = await program.reLaunch(HOME_PATH)
-		await page.waitFor(700)
-		lifeCycleNum = await page.callMethod('getLifeCycleNum')
-		expect(lifeCycleNum).toBe(1100)
-	})
-	it('onLastPageBackPress', async () => {
-		if (process.env.uniTestPlatformInfo.startsWith('android')) {
-			page = await program.navigateBack()
-			await page.waitFor(700)
-			lifeCycleNum = await page.callMethod('getLifeCycleNum')
-			expect(lifeCycleNum).toBe(100)
-		}
-		await page.callMethod('setLifeCycleNum', 0)
-		lifeCycleNum = await page.callMethod('getLifeCycleNum')
-		expect(lifeCycleNum).toBe(0)
-	})
-})
-
 describe('page-lifecycle', () => {
+  beforeAll(async () => {
+    page = await program.reLaunch(HOME_PATH)
+    await page.waitFor(700)
+    await page.callMethod('setLifeCycleNum', 0)
+  });
 	afterAll(async () => {
 		const resetLifecycleNum = 1100
 		await page.callMethod('setLifeCycleNum', resetLifecycleNum)
@@ -37,22 +22,22 @@ describe('page-lifecycle', () => {
 		await page.waitFor(700)
 		lifeCycleNum = await page.callMethod('getLifeCycleNum')
 		expect(lifeCycleNum).toBe(130)
-		await page.callMethod('setLifeCycleNum', 0)
+		await page.callMethod('pageSetlifeCycleNum', 0)
 	})
 	it('onPullDownRefresh', async () => {
 		await page.callMethod('pullDownRefresh')
 		await page.waitFor(1500)
 		lifeCycleNum = await page.callMethod('getLifeCycleNum')
 		expect(lifeCycleNum).toBe(10)
-		await page.callMethod('setLifeCycleNum', 0)
+		await page.callMethod('pageSetlifeCycleNum', 0)
 	})
 	it('onPageScroll onReachBottom', async () => {
 		await program.pageScrollTo(2000)
-		const data = await page.data()
-		expect(data.isScrolled).toBe(true)
+		const isScrolled = await page.callMethod('getIsScrolled')
+		expect(isScrolled).toBe(true)
 		lifeCycleNum = await page.callMethod('getLifeCycleNum')
 		expect(lifeCycleNum).toBe(10)
-		await page.callMethod('setLifeCycleNum', 0)
+		await page.callMethod('pageSetlifeCycleNum', 0)
 	})
 	it('onHide', async () => {
 		page = await program.navigateTo(INTER_PAGE_PATH)
