8c1900b9e7faba993ad6274a7fe96c00b78e8a4b
wangjinxin613 <1749574663@qq.com>
2024-01-25 17:42:58 +0800

    test(watch-effect)：调整示例兼容 safari

--- pages/composition-api/reactivity/watch-effect/watch-effect.test.js
+++ pages/composition-api/reactivity/watch-effect/watch-effect.test.js
@@ -1,6 +1,7 @@
 const PAGE_PATH = '/pages/composition-api/reactivity/watch-effect/watch-effect'
 
 describe('watchEffect', () => {
+  const isSafari = process.env.uniTestPlatformInfo.indexOf('safari') > -1
   let page = null
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
@@ -76,7 +77,7 @@ describe('watchEffect', () => {
     const objBool = await page.$('#obj-bool')
     expect(await objBool.text()).toBe('obj.bool: false')
     const objArr = await page.$('#obj-arr')
-    expect(await objArr.text()).toBe('obj.arr: [0]')
+    expect(await objArr.text()).toBe(isSafari ? 'obj.arr: [ 0]' : 'obj.arr: [0]')
 
     const watchObjRes = await page.$('#watch-obj-res')
     if (process.env.uniTestPlatformInfo.startsWith('android')) {
@@ -104,7 +105,7 @@ describe('watchEffect', () => {
     expect(await objStr.text()).toBe('obj.str: num: 1')
     expect(await objNum.text()).toBe('obj.num: 1')
     expect(await objBool.text()).toBe('obj.bool: true')
-    expect(await objArr.text()).toBe('obj.arr: [0,1]')
+    expect(await objArr.text()).toBe(isSafari ? 'obj.arr: [ 0, 1]' : 'obj.arr: [0,1]')
 
     if (process.env.uniTestPlatformInfo.startsWith('android')) {
       expect(await watchObjRes.text()).toBe(
