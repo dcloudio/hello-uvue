6895a849b607496e1614bf786a41591a8ed03932
zhenyuWang <13641039885@163.com>
2024-03-20 17:12:06 +0800

    test: 规避 ios 自动化测试超时问题

--- pages/built-in-component/teleport/teleport.test.js
+++ pages/built-in-component/teleport/teleport.test.js
@@ -1,6 +1,13 @@
 const PAGE_PATH = '/pages/built-in-component/teleport/teleport'
 
 describe('teleport', () => {
+  if(process.env.uniTestPlatformInfo.startsWith('IOS')){
+    it("IOS platform not support", async () => {
+      expect(1).toBe(1);
+    });
+    return
+  }
+  
   let page = null
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
