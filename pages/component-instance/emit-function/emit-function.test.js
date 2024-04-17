ba25eab3b48527cb9663403cecd6f41bb1d0a01f
DCloud_LXH <283700113@qq.com>
2024-04-17 17:54:38 +0800

    refactor(component-instance): emit

--- pages/component-instance/emit-function/emit-function.test.js
+++ pages/component-instance/emit-function/emit-function.test.js
@@ -1,13 +1,24 @@
-const PAGE_PATH = '/pages/component-instance/emit-function/emit-function'
+const PAGE_PATH = '/pages/component-instance/emit-function/emit-function-options'
+const PAGE_COMPOSITION_PATH = '/pages/component-instance/emit-function/emit-function-composition'
 
 describe('$emit()', () => {
   let page
-  beforeAll(async () => {
+  it('$emit() 选项式 API 事件生效', async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor(500)
+    const beforeValue = await page.data('value')
+    const btn = await page.$('.call-parent-btn')
+
+    btn.tap()
+
+    const afterValue = await page.data('value')
+
+    expect(beforeValue).not.toBe(afterValue)
   })
 
-  it('$emit() 事件生效', async () => {
+  it('$emit() 组合式 API 事件生效', async () => {
+    page = await program.reLaunch(PAGE_COMPOSITION_PATH)
+    await page.waitFor(500)
     const beforeValue = await page.data('value')
     const btn = await page.$('.call-parent-btn')
 
