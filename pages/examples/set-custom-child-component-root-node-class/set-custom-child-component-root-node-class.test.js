fa5dc4833bb0f22cae5357d7e8e4c7a0b9a58297
DCloud_LXH <283700113@qq.com>
2024-04-17 15:18:16 +0800

    feat: example

--- pages/examples/set-custom-child-component-root-node-class/set-custom-child-component-root-node-class.test.js
+++ pages/examples/set-custom-child-component-root-node-class/set-custom-child-component-root-node-class.test.js
@@ -1,14 +1,19 @@
-const PAGE_PATH =
-  '/pages/examples/set-custom-child-component-root-node-class/set-custom-child-component-root-node-class'
+const PAGE_PATH = '/pages/examples/set-custom-child-component-root-node-class/set-custom-child-component-root-node-class-options'
+const PAGE_PATH_COMPOSITION = '/pages/examples/set-custom-child-component-root-node-class/set-custom-child-component-root-node-class-composition'
 
 describe('自定义组件中使用 class 定制另一个自定义组件根节点样式', () => {
   let page
-  beforeAll(async () => {
+
+  it('set-custom-child-component-root-node-class-options Screenshot', async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor('view')
+    const image = await program.screenshot()
+    expect(image).toMatchImageSnapshot()
   })
 
-  it('screenshot', async () => {
+  it('set-custom-child-component-root-node-class-options Screenshot', async () => {
+    page = await program.reLaunch(PAGE_PATH_COMPOSITION)
+    await page.waitFor('view')
     const image = await program.screenshot()
     expect(image).toMatchImageSnapshot()
   })
