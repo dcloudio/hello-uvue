8e5193aa28cfe89f9ef860f634aa26d7bc868949
DCloud_LXH <283700113@qq.com>
2024-04-17 11:41:58 +0800

    chore: format code

--- pages/built-in/component/component/component.test.js
+++ pages/built-in/component/component/component.test.js
@@ -2,33 +2,33 @@ const PAGE_OPTIONS = '/pages/built-in/component/component/component-options'
 const PAGE_COMPOSITION = '/pages/built-in/component/component/component-composition'
 
 describe('built-in/component', () => {
-	let page
-	const test = async () => {
-		await page.waitFor('view')
-		let fooList = await page.$$('.component-foo')
-		expect(fooList.length).toBe(2)
-		expect(await fooList[0].text()).toBe('this is component Foo')
-		expect(await fooList[1].text()).toBe('this is component Foo')
+  let page
+  const test = async () => {
+    await page.waitFor('view')
+    let fooList = await page.$$('.component-foo')
+    expect(fooList.length).toBe(2)
+    expect(await fooList[0].text()).toBe('this is component Foo')
+    expect(await fooList[1].text()).toBe('this is component Foo')
 
-		let barList = await page.$$('.component-bar')
-		expect(barList.length).toBe(0)
+    let barList = await page.$$('.component-bar')
+    expect(barList.length).toBe(0)
 
-		await page.callMethod('changeCurrentComponent')
+    await page.callMethod('changeCurrentComponent')
 
-		fooList = await page.$$('.component-foo')
-		expect(fooList.length).toBe(0)
+    fooList = await page.$$('.component-foo')
+    expect(fooList.length).toBe(0)
 
-		barList = await page.$$('.component-bar')
-		expect(barList.length).toBe(2)
-		expect(await barList[0].text()).toBe('this is component Bar')
-		expect(await barList[1].text()).toBe('this is component Bar')
-	}
-	it('component Options API', async () => {
-		page = await program.reLaunch(PAGE_OPTIONS)
-		await test()
-	})
-	it('component Composition API', async () => {
-		page = await program.reLaunch(PAGE_COMPOSITION)
-		await test()
-	})
+    barList = await page.$$('.component-bar')
+    expect(barList.length).toBe(2)
+    expect(await barList[0].text()).toBe('this is component Bar')
+    expect(await barList[1].text()).toBe('this is component Bar')
+  }
+  it('component Options API', async () => {
+    page = await program.reLaunch(PAGE_OPTIONS)
+    await test()
+  })
+  it('component Composition API', async () => {
+    page = await program.reLaunch(PAGE_COMPOSITION)
+    await test()
+  })
 });
