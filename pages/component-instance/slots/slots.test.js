ce42d1060bbb79194837c8cf124f8dd7de3d318a
DCloud_LXH <283700113@qq.com>
2024-04-17 15:58:17 +0800

    refactor(component-instance): $slots

--- pages/component-instance/slots/slots.test.js
+++ pages/component-instance/slots/slots.test.js
@@ -1,16 +1,22 @@
-const PAGE_PATH = '/pages/component-instance/slots/slots'
+const PAGE_PATH = '/pages/component-instance/slots/slots-options'
+const PAGE_COMPOSITION_PATH = '/pages/component-instance/slots/slots-composition'
 
 describe('$slots', () => {
   let page
-  beforeAll(async () => {
+
+  it('$slots Options API 生效', async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor(500)
-  })
-
-  it('$slots 生效', async () => {
     const slotComp = await page.$('.slot-comp')
     const hasSlots = await slotComp.callMethod('hasSlots')
+    expect(hasSlots).toBe(true)
+  })
 
+  it('$slots Composition API 生效', async () => {
+    page = await program.reLaunch(PAGE_COMPOSITION_PATH)
+    await page.waitFor(500)
+    const slotComp = await page.$('.slot-comp')
+    const hasSlots = await slotComp.callMethod('hasSlots')
     expect(hasSlots).toBe(true)
   })
 })
