b41b9a01658cfe401c57dc9c3f28e5212f0db817
DCloud_LXH <283700113@qq.com>
2024-04-17 17:33:05 +0800

    refactor(component-instance): refs

--- pages/component-instance/refs/refs.test.js
+++ pages/component-instance/refs/refs.test.js
@@ -1,15 +1,28 @@
-const PAGE_PATH = '/pages/component-instance/refs/refs'
+const PAGE_PATH = '/pages/component-instance/refs/refs-options'
+const PAGE_COMPOSITION_PATH = '/pages/component-instance/refs/refs-composition'
 
 describe('$refs', () => {
   let page
-  beforeAll(async () => {
+
+  it('$refs 选项式 API 属性生效', async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor(500)
-  })
 
-  it('$refs 属性生效', async () => {
+    expect.assertions(2)
     const data = await page.data()
 
     expect(data.existRef).toBe(true)
-  });
+    expect(data.exisChildRef).toBe(true)
+  })
+
+  it('$refs 组合式 API 属性生效', async () => {
+    page = await program.reLaunch(PAGE_COMPOSITION_PATH)
+    await page.waitFor(500)
+
+    expect.assertions(2)
+    const data = await page.data()
+    console.log('data: ',data);
+    expect(data.refObject.existRef).toBe(true)
+    expect(data.refObject.exisChildRef).toBe(true)
+  })
 })
