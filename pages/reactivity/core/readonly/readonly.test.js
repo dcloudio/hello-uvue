389ac221d6d9ec368891c837a5d711ee069fa002
zhenyuWang <13641039885@163.com>
2024-04-20 18:39:33 +0800

    refactor(reactivity): readonly

--- pages/reactivity/core/readonly/readonly.test.js
+++ pages/reactivity/core/readonly/readonly.test.js
@@ -1,4 +1,4 @@
-const PAGE_PATH = '/pages/composition-api/reactivity/readonly/readonly'
+const PAGE_PATH = '/pages/reactivity/core/readonly/readonly'
 
 describe('ref', () => {
   let page = null
@@ -8,37 +8,37 @@ describe('ref', () => {
   })
   it('basic', async () => {
     const dataStr = await page.$('#data-str')
-    expect(await dataStr.text()).toBe('data.str: default str')
+    expect(await dataStr.text()).toBe('default str')
     const dataNum = await page.$('#data-num')
-    expect(await dataNum.text()).toBe('data.num: 0')
+    expect(await dataNum.text()).toBe('0')
     const dataArr = await page.$('#data-arr')
-    expect(await dataArr.text()).toBe('data.arr: ["a","b","c"]')
+    expect(await dataArr.text()).toBe('["a","b","c"]')
 
     const readonlyDataStr = await page.$('#readonly-data-str')
-    expect(await readonlyDataStr.text()).toBe('readonly data.str: default str')
+    expect(await readonlyDataStr.text()).toBe('default str')
     const readonlyDataNum = await page.$('#readonly-data-num')
-    expect(await readonlyDataNum.text()).toBe('readonly data.num: 0')
+    expect(await readonlyDataNum.text()).toBe('0')
     const readonlyDataArr = await page.$('#readonly-data-arr')
-    expect(await readonlyDataArr.text()).toBe('readonly data.arr: ["a","b","c"]')
+    expect(await readonlyDataArr.text()).toBe('["a","b","c"]')
 
     const updateDataBtn = await page.$('#update-data-btn')
     await updateDataBtn.tap()
 
-    expect(await dataStr.text()).toBe('data.str: new str')
-    expect(await dataNum.text()).toBe('data.num: 1')
-    expect(await dataArr.text()).toBe('data.arr: ["a","b","c","d"]')
-    expect(await readonlyDataStr.text()).toBe('readonly data.str: new str')
-    expect(await readonlyDataNum.text()).toBe('readonly data.num: 1')
-    expect(await readonlyDataArr.text()).toBe('readonly data.arr: ["a","b","c","d"]')
+    expect(await dataStr.text()).toBe('new str')
+    expect(await dataNum.text()).toBe('1')
+    expect(await dataArr.text()).toBe('["a","b","c","d"]')
+    expect(await readonlyDataStr.text()).toBe('new str')
+    expect(await readonlyDataNum.text()).toBe('1')
+    expect(await readonlyDataArr.text()).toBe('["a","b","c","d"]')
 
     const updateReadonlyDataBtn = await page.$('#update-readonly-data-btn')
     await updateReadonlyDataBtn.tap()
 
-    expect(await dataStr.text()).toBe('data.str: new str')
-    expect(await dataNum.text()).toBe('data.num: 1')
-    expect(await dataArr.text()).toBe('data.arr: ["a","b","c","d"]')
-    expect(await readonlyDataStr.text()).toBe('readonly data.str: new str')
-    expect(await readonlyDataNum.text()).toBe('readonly data.num: 1')
-    expect(await readonlyDataArr.text()).toBe('readonly data.arr: ["a","b","c","d"]')
+    expect(await dataStr.text()).toBe('new str')
+    expect(await dataNum.text()).toBe('1')
+    expect(await dataArr.text()).toBe('["a","b","c","d"]')
+    expect(await readonlyDataStr.text()).toBe('new str')
+    expect(await readonlyDataNum.text()).toBe('1')
+    expect(await readonlyDataArr.text()).toBe('["a","b","c","d"]')
   })
 })
