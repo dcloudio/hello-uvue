dc38f9b958beee1328a9ad5b49a9f492fb195918
zhenyuWang <13641039885@163.com>
2024-04-20 12:58:16 +0800

    refactor(component instance): defineExpose

--- pages/component-instance/define-expose/define-expose.test.js
+++ pages/component-instance/define-expose/define-expose.test.js
@@ -1,20 +1,22 @@
-const PAGE_PATH = '/pages/composition-api/basic/define-expose/define-expose'
+const PAGE_PATH = '/pages/component-instance/define-expose/define-expose'
 
 describe('defineExpose', () => {
   let page = null
+  
   beforeAll(async () => {
     page = await program.reLaunch(PAGE_PATH)
     await page.waitFor('view')
   })
+  
   it('basic', async () => {
     const fooStr = await page.$('#foo-str')
-    expect(await fooStr.text()).toBe('str from component Foo: foo str')
+    expect(await fooStr.text()).toBe('foo str')
     const fooNum = await page.$('#foo-num')
-    expect(await fooNum.text()).toBe('num from component Foo: 0')
+    expect(await fooNum.text()).toBe('0')
 
     const incrementBtn = await page.$('.increment-btn')
     await incrementBtn.tap()
 
-    expect(await fooNum.text()).toBe('num from component Foo: 1')
+    expect(await fooNum.text()).toBe('1')
   })
 })
