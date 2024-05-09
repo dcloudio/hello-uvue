28380dacdead9922514b0d730dfc73ed6010f12a
zhenyuWang <13641039885@163.com>
2024-05-09 11:51:17 +0800

    feat(render function): cloneVNode

---
+++ pages/render-function/cloneVNode/cloneVNode.test.js
@@ -0,0 +1,30 @@
+const OPTIONS_PAGE_PATH = '/pages/render-function/cloneVNode/cloneVNode-options'
+const COMPOSITION_PAGE_PATH = '/pages/render-function/cloneVNode/cloneVNode-composition'
+
+describe('cloneVNode', () => {
+  if (process.env.uniTestPlatformInfo.toLocaleLowerCase().startsWith('ios')) {
+    // TODO: ios options API 合并属性无效, composition API 页面空白
+    it("IOS platform not support", async () => {
+      expect(1).toBe(1);
+    });
+    return
+  }
+
+  let page = null
+  const test = async (pagePath) => {
+    page = await program.reLaunch(pagePath)
+    // 因为 web 端无法获取, 未使用 waitFor view
+    await page.waitFor(1000)
+
+    const image = await program.screenshot();
+    expect(image).toSaveImageSnapshot();
+  }
+
+  it('cloneVNode options API', async () => {
+    await test(OPTIONS_PAGE_PATH)
+  })
+
+  it('cloneVNode composition API', async () => {
+    await test(COMPOSITION_PAGE_PATH)
+  })
+})
