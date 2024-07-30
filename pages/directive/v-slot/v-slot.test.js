6f3413622fbadfa1fb8fa2883029f2574cb95305
zhenyuWang <13641039885@163.com>
2024-07-30 16:52:53 +0800

    feat(v-slot): 补充 v-for 动态插槽名示例

--- pages/directive/v-slot/v-slot.test.js
+++ pages/directive/v-slot/v-slot.test.js
@@ -11,8 +11,18 @@ describe('v-slot', () => {
     const slotHeader = await page.$('#slot-header')
     expect(await slotHeader.text()).toBe('foo msg')
 
-    const slotContent = await page.$('#slot-default')
-    expect(await slotContent.text()).toBe('0')
+    const slotDefault = await page.$('#slot-default')
+    expect(await slotDefault.text()).toBe('0')
+    
+    const slotNum1 = await page.$('#slot-num1')
+    expect(await slotNum1.text()).toBe('0')
+    const slotNum2 = await page.$('#slot-num2')
+    expect(await slotNum2.text()).toBe('0')
+    
+    const slotMsgTrue = await page.$('#slot-msg-true')
+    expect(await slotMsgTrue.text()).toBe('foo msg')
+    const slotMsgFalse = await page.$('#slot-msg-false')
+    expect(slotMsgFalse).toBe(null)
 
     const slotFooter = await page.$('#slot-footer')
     expect(await slotFooter.text()).toBe('["a","b","c"]')
