44e2cec786810dc029a81fefff6c0e90e9d09507
zhenyuWang <13641039885@163.com>
2024-12-12 15:23:09 +0800

    test: 优化自动化测试顺序

--- testSequencer.js
+++ testSequencer.js
@@ -1,16 +1,25 @@
 const Sequencer = require("@jest/test-sequencer").default
-const sortTestFilePaths = [
+const startTestFilePaths = [
   "pages/App.test.js",
+]
+const endTestFilePaths = [
+  "pages/error/runtime-error/runtime-error.test.js",
+  "pages/error/throw-error/throw-error.test.js",
 ]
 class CustomSequencer extends Sequencer {
   sort(tests) {
     // 测试例排序
-    const sortedTests = sortTestFilePaths
+    const startTests = startTestFilePaths
       .map((filePath) => {
         return tests.find((test) => test.path.endsWith(filePath))
       })
       .filter(Boolean)
-    return [...new Set([...sortedTests, ...tests])]
+    const endTests = endTestFilePaths
+      .map((filePath) => {
+        return tests.find((test) => test.path.endsWith(filePath))
+      })
+      .filter(Boolean)
+    return [...new Set([...startTests, ...tests, ...endTests])]
   }
 }
 
