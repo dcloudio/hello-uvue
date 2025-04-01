f62ce77115b763d64660d948a5ffc2e5cc71270b
jixinbao <jixinbao@dcloud.io>
2025-04-01 14:28:55 +0800

    test: 鸿蒙平台无需测试 兼容组件返回值

--- pages/component-instance/methods/call-method-easycom-uni-modules-props-emits.test.js
+++ pages/component-instance/methods/call-method-easycom-uni-modules-props-emits.test.js
@@ -5,11 +5,13 @@ const platformInfo = process.env.uniTestPlatformInfo.toLowerCase()
 const isIOS = platformInfo.startsWith('ios')
 const isWeb = platformInfo.startsWith('web')
 const isMP = platformInfo.startsWith('mp')
+const isHarmony = platformInfo.startsWith('harmony')
 
 describe('call method easycom uni modules', () => {
   if (
     isMP ||
     isWeb ||
+    isHarmony ||
     (
     isIOS &&
       (
@@ -31,7 +33,7 @@ describe('call method easycom uni modules', () => {
 
     await page.callMethod('onButtonClick')
     await page.waitFor(1500)
-    
+
     const resStr1 = await page.$("#isNumListValid")
     const resStr2 = await page.$("#isObjListValid")
     expect(await resStr1.text()).toBe(`true`)
